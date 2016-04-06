87ab97bafebc1e84818758e3109adf32ed70603d
yuxin-l <yuxin-l@sohu.com>
2016-04-06 08:23:07 +0800

    0823

--- mouseevent/game.ts
+++ mouseevent/game.ts
@@ -1,29 +1,62 @@
 
+var human = new render.DisplayObjectContainer();
+human.x = -50;
+human.y = -120;
 var humanContainer = new render.DisplayObjectContainer();
 var head = new render.Bitmap();
-head.x = 100;
-head.source = "wander-icon.jpg";
-humanContainer.addChild(head);
+head.x = 10;
+head.y = -90;
+var trunk = new render.Bitmap();
+trunk.x = 20;
+var left_arm = new render.Bitmap();
+left_arm.x = -10;
+left_arm.y = 30;
+var right_arm = new render.Bitmap();
+right_arm.x = 95;
+right_arm.y = 30;
+var left_leg = new render.Bitmap();
+left_leg.x = 0;
+left_leg.y = 130;
+var right_leg = new render.Bitmap();
+right_leg.x = 60;
+right_leg.y = 120;
+
+head.source = "head.png";
+trunk.source = "trunk.png";
+left_arm.source = "left_arm.png";
+right_arm.source = "right_arm.png";
+left_leg.source = "left_leg.png";
+right_leg.source = "right_leg.png";
+
+humanContainer.addChild(human);
+human.addChild(left_arm);
+human.addChild(right_arm);
+human.addChild(left_leg);
+human.addChild(right_leg);
+human.addChild(head);
+human.addChild(trunk);
 
 
 var renderCore = new render.RenderCore();
-renderCore.start(humanContainer, ["wander-icon.jpg"]);
+
+renderCore.start(humanContainer, ["left_arm.png","right_arm.png", "left_leg.png", "right_leg.png", "head.png", "trunk.png"]);
+
 
 class HumanBody extends Body {
-    
-    
-    vx:number = 5;
-    
+
 
     onTicker(duringTime: number) {
-        this.x = 100;//+= duringTime * this.vx;
-        this.y = 100;
+
+         this.x += this.vx*duringTime;
+         this.rotation += Math.PI*duringTime;
 
     }
 }
 
 var ticker = new Ticker();
 var body = new HumanBody(humanContainer);
+body.vx = 5;
+body.y = 200; 
 ticker.start([body]);
 
 
